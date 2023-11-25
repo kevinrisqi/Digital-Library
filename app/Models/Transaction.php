0c45ceaaf6c3193bebfaad607cc80e7fa083cc57
kevinrisqi <kevinrisqi22@gmail.com>
2023-11-25 20:27:12 +0700

    create book, category, transaction, user model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'borrowed_date',
+        'returned_date',
+        'status',
+        'quantity',
+    ];
+
+    /**
+     * Get the user that owns the transaction.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the book associated with the transaction.
+     */
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+}
