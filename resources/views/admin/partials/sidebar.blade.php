62decf13b5e12c2b1bb8ee1c777de69d09b66d74
kevinrisqi <kevinrisqi22@gmail.com>
2023-11-27 09:24:06 +0700

    set manually adminlte

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/adminlte/partials/sidebar.blade.php -->
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Sidebar</h3>
+    </div>
+    <div class="card-body">
+        <!-- Sidebar content goes here -->
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\AdminLoginController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,10 +29,12 @@ Route::resource('books', BookController::class)->except(['show']);
 Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
 
 Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+
     Route::get('/login', [AdminLoginController::class, 'showLoginForm'])->name('admin.login');
     Route::post('/login', [AdminLoginController::class, 'login']);
     Route::post('/logout', [AdminLoginController::class, 'logout'])->name('admin.logout');
-    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+
     Route::get('/index', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
     Route::post('/store', [AdminController::class, 'store'])->name('admin.store');
@@ -45,3 +48,6 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth:admin'])->group(function () {
     // Your admin-only routes here...
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
