192577eb9e1a6a698cef999fda057152eaba82e9
yogaibnu <yogaibnudinataa@gmail.com>
2023-12-02 17:16:19 +0700

    update footer

--- resources/views/admin/layout/admin.blade.php
+++ resources/views/admin/layout/admin.blade.php
@@ -53,7 +53,7 @@
                     </div>
                     <div class="info">
                         <!-- Your user information -->
-                        <a href="#" class="d-block">{{ $name }}</a>
+                        {{ $name }}
                     </div>
                 </div>
 
@@ -136,10 +136,10 @@
 
         <!-- Main Footer -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright &copy; 2023 <a href="{{ route('admin.dashboard.index') }}">Perpustakaan Digital</a>.</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+                <b>Version</b> 0.1.0
             </div>
         </footer>
     </div>
